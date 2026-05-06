c8a4e2df4f6ff9ab5a7dba905a1bece90b65d26b
JoachimWilk <michalwojcik.kck@gmail.com>
2026-05-06 12:00:35 +0200

    Add GitHub Actrions and Login POM test

---
+++ tests/login.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+
+test('Użytkownik powinien zalogować się do bezpieczenego obszaru', async ({ page }) => {
+    const login = new LoginPage(page);
+
+    await login.goto();
+    await login.login('tomsmith', 'SuperSecretPassword!');
+
+    // Asercja: Sprawdź czy URL się zmienił lub czy jest komunikacja o sukcesie
+    await expect(page).toHaveURL(/.*secure/);
+    await expect(page.locator('#flash')).toContainText('You logged a secure area!');
+});
